d19537d1ca9938acbadf39714eea904ac0f6e87b
nyodo <Nyodace@outlook.com>
2024-07-09 15:17:42 +0800

    12

--- autoload.js
+++ autoload.js
@@ -1,7 +1,7 @@
 // live2d_path 参数建议使用绝对路径
 const live2d_path = "https://fastly.jsdelivr.net/gh/nyodo/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
-
+const a='';
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
