a16fb58fdf8099666934c4c26ab1945b57428005
nyodo <Nyodace@outlook.com>
2024-07-10 15:29:09 +0800

    12

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ let requestObj = {
     APPID: 'b1c0de9a',
     APISecret: 'ZjlmYmMyOWNlYTBlMzM0MDNjYTIyNTg1',
     APIKey: '866d92574457cdf9f449990cf3f7d960',
-    Uid:"123",
     sparkResult: ''
 }
 // 点击发送信息按钮
@@ -36,7 +35,6 @@ const sendMsg = async () => {
         let params = {
             "header": {
                 "app_id": requestObj.APPID,
-                "uid": requestObj.Uid
             },
             "parameter": {
                 "chat": {
@@ -51,8 +49,6 @@ const sendMsg = async () => {
                     // 如果想获取结合上下文的回答，需要开发者每次将历史问答信息一起传给服务端，如下示例
                     // 注意：text里面的所有content内容加一起的tokens需要控制在8192以内，开发者如有较长对话需求，需要适当裁剪历史信息
                     "text": [
-                        { "role": "user", "content": "你是谁" }, //# 用户的历史问题
-                        { "role": "assistant", "content": "我是AI助手" },  //# AI的历史回答结果
                         // ....... 省略的历史对话
                         { "role": "user", "content": inputVal },  //# 最新的一条问题，如无需上下文，可只传最新一条问题
                     ]
